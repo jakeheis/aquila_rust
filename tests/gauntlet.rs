d7e251733bf75abb52ffea9cef434dc1123b36f6
Jake Heiser <jakeheiser1@gmail.com>
2020-06-25 16:12:13 -0600

    Failure zones in tests

--- tests/gauntlet.rs
+++ tests/gauntlet.rs
@@ -1,6 +1,9 @@
-use std::fs;
+use std::fs::{self, File};
 use std::path::PathBuf;
 use std::process::Command;
+use std::io::Write;
+use std::path::Path;
+use aquila::diagnostic::*;
 
 mod common;
 use common::*;
@@ -19,56 +22,139 @@ fn gauntlet() -> Result<(), &'static str> {
 }
 
 fn test_file(file: PathBuf) -> Result<(), &'static str> {
-    let (reporter, mut diagnostics) = TestReporter::new();
-
     let read = fs::read_to_string(file.clone()).unwrap();
     let lines: Vec<_> = read.lines().collect();
-    if lines[0] == "// skip" {
+
+    if lines[0] == "/// skip" {
         println!("Skipping");
         return Ok(());
     }
 
-    let mut expected_output: Vec<String> = Vec::new();
+    let mut main: Vec<String> = Vec::new();
+    let mut expected_lines: Vec<String> = Vec::new();
+    let mut fail_zones: Vec<Vec<String>> = Vec::new();
+    let mut in_failable_zone = false;
+
     for line in lines {
         if line.starts_with("/// ") {
-            let output = &line[4..];
-            expected_output.push(String::from(output));
+            let chopped_line = &line[4..];
+            if chopped_line.starts_with("->") {
+                if in_failable_zone {
+                    panic!()
+                }
+                let expected = &line[7..];
+                expected_lines.push(String::from(expected));
+            } else if chopped_line.starts_with("error") {
+                if in_failable_zone {
+                    panic!()
+                }
+                in_failable_zone = true;
+                fail_zones.push(Vec::new());
+            } else if chopped_line.starts_with("end") {
+                if !in_failable_zone {
+                    panic!()
+                }
+                in_failable_zone = false;
+            } else {
+                panic!()
+            }
+        } else {
+            let line = String::from(line);
+            if in_failable_zone {
+                fail_zones.last_mut().unwrap().push(line);
+            } else {
+                main.push(line);
+            }
         }
     }
+
+    fs::create_dir_all("/tmp/aquila").unwrap();
+
+    let main_file_path = format!("/tmp/aquila/{}_main.aq", file.file_stem().unwrap().to_str().unwrap());
+    let mut main_file = File::create(main_file_path.clone()).unwrap();
+    for line in &main {
+        writeln!(main_file, "{}", line).unwrap();
+    }
+    expect_success(Path::new(&main_file_path).to_path_buf(), expected_lines)?;
+
+    for (index, fail_zone) in fail_zones.iter().enumerate() {
+        let file_path = format!("/tmp/aquila/{}_fail_{}.aq", file.file_stem().unwrap().to_str().unwrap(), index);
+        let mut file = File::create(file_path.clone()).unwrap();
+        for line in &main {
+            writeln!(file, "{}", line).unwrap();
+        }
+        for line in fail_zone {
+            writeln!(file, "{}", line).unwrap();
+        }
+        expect_failure(Path::new(&file_path).to_path_buf(), index)?;
+    }
+
+    println!("fail_zones {:#?}", fail_zones);
+
+    Ok(())
+}
+
+fn expect_success(file: PathBuf, expected_output: Vec<String>) -> Result<(), &'static str> {
+    let (output, diagnostics) = run_file(file);
+
+    if !diagnostics.is_empty() {
+        println!("Diagnostics:");
+        for diag in diagnostics {
+            println!("{}", diag);
+        }
+        return Err("Expected no diagnostics");
+    }
+
+    if output.lines().count() != expected_output.len() {
+        println!("Got: {:?}", output.lines().collect::<Vec<_>>());
+        println!("Expected: {:?}", expected_output);
+        return Err("Wrong number of output lines");
+    }
+
+    for (index, (got, expected)) in output.lines().zip(expected_output).enumerate() {
+        if got != expected {
+            println!("Expected line {}: expected '{}', got '{}'", index + 1, expected, got);
+            return Err("Invalid output");
+        }
+    }
+
+    Ok(())
+}
+
+fn expect_failure(file: PathBuf, index: usize) -> Result<(), &'static str> {
+    let (_, diagnostics) = run_file(file);
+
+    if diagnostics.is_empty() {
+        println!("Expected diagnostics in failure zone {}", index + 1);
+        Err("Expected diagnostics")
+    } else {
+        Ok(())
+    }
+}
+
+fn run_file(file: PathBuf) -> (String, Vec<Diagnostic>) {
+    let (reporter, mut diagnostics) = TestReporter::new();
 
     let source = aquila::source::file(file.to_str().unwrap());
     match aquila::run_with_reporter(source, reporter) {
-        Ok(_) => assert_eq!(diagnostics.unwrap().len(), 0),
-        Err(message) => {
-            println!("Diagnostics:");
-            for diag in diagnostics.unwrap() {
-                println!("{}", diag);
-            }
-            return Err(message);
-        }
-    }
+        Ok(_) => {
+            assert_eq!(diagnostics.unwrap().len(), 0);
 
-    if !expected_output.is_empty() {
-        let output = Command::new("/Users/jakeheiser/Desktop/Projects/Rust/aquila/build/program")
+            let output = Command::new("/Users/jakeheiser/Desktop/Projects/Rust/aquila/build/program")
             .output()
             .unwrap();
 
-        let got_lines = String::from_utf8(output.stdout).unwrap();
+            let got_lines = String::from_utf8(output.stdout).unwrap();
 
-        if !output.status.success() {
-            println!("Output:\n{}", got_lines);
-            return Err("Run failed");
-        }
-
-        for (index, (got, expected)) in got_lines.lines().zip(expected_output).enumerate() {
-            if got != expected {
-                println!("Line {}: expected '{}', got '{}'", index + 1, expected, got);
-                return Err("Invalid output");
+            if !output.status.success() {
+                println!("Output:\n{}", got_lines);
+                panic!()
             }
-        }
 
-        Ok(())
-    } else {
-        Ok(())
+            (got_lines, Vec::new())
+        },
+        Err(_) => {
+            (String::new(), diagnostics.unwrap())
+        }
     }
 }
